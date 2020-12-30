e0fd87df5e1621d33bcb3fdc02d130ed471b25c6
Vladislav Ainshtein <vladikasik@MacBook-Pro-Apple.local>
2020-12-30 19:14:27 +0300

    mainapp

--- dj_pr/main/views.py
+++ dj_pr/main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here
+def index(request):
+	return render(request, 'main/index.html')
 
-# Create your views here.
+def about(request):
+	return HttpResponse("<h4>About us</h4>")
+	
+	
