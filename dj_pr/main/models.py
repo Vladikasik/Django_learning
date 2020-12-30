c16bd18e32a88221fe5f4a2ab53cbd9ba64ee733
Vladislav Ainshtein <vladikasik@MacBook-Pro-Apple.local>
2020-12-30 21:07:21 +0300

    html shablony

--- dj_pr/main/models.py
+++ dj_pr/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+	title = models.CharField('Название', max_length=66)
+	task_text = models.TextField('Описание')
+
+	def __str__(self):
+		return self.title
+
--- dj_pr/main/views.py
+++ dj_pr/main/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Task
+
 # Create your views here
 def index(request):
-	return render(request, 'main/index.html')
+	tasks = Task.objects.all()
+	return render(request, 'main/index.html', {'tasks': tasks})
 
 def about(request):
 	return render(request, 'main/about.html')
 	
-	
+
