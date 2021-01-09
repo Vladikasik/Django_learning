2d4285f0080600c5ba81ecf0268397ff12095042
Vladisav <vsakayafignavlad@gmail.com>
2021-01-09 16:29:19 +0300

    Form working

--- dj_pr/main/forms.py
+++ dj_pr/main/forms.py
@@ -1,9 +1,19 @@
 from .models import Task
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, Textarea
 
 
 class TaskForm(ModelForm):
 
 	class Meta:
 		model = Task
-		fields = ["title", "task"]
+		fields = ["title", "task_text"]
+		widgets = {
+		"title": TextInput(attrs={
+			"class": 'form-control',
+			"placeholder": 'Введите название'
+			}),
+		"task_text": Textarea(attrs={
+			"class": 'form-control',
+			"placeholder": 'Введите описание'
+			}),
+		}
--- dj_pr/main/views.py
+++ dj_pr/main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 # Create your views here
 def index(request):
@@ -10,6 +11,18 @@ def about(request):
 	return render(request, 'main/about.html')
 
 def create(request):
-	return render(request, 'main/create.html')
+	if request.method == "POST":
+		form = TaskForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('home')
+		else:
+			error = 'Ошибка при заполнении формы'
+
+	form = TaskForm()
+	context = {
+		'form': form
+	}
+	return render(request, 'main/create.html', context)
 	
 
