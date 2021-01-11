065a3790b949993a604763b3b087b700c819ee16
Vlad <vsakayafignavlad@gmail.com>
2021-01-11 23:26:41 +0300

    Settings install app

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'articles.apps.ArticlesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
