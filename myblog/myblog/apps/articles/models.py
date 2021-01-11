39b90e07a563f91403eac0ff4cb4cdade638bfd8
Vlad <vsakayafignavlad@gmail.com>
2021-01-11 23:23:50 +0300

    mistakes

--- myblog/myblog/apps/articles/models.py
+++ myblog/myblog/apps/articles/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Article(models.Model):
     article_title = models.CharField('Title name', max_length=200)
     article_text = models.TextField('Title text')
-    article_author = models.CharField('Author Name')
     pub_date = models.DateTimeField('publication date')
 
 
