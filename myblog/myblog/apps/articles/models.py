fde14e5228a6c957ffc9af8826b69b98ac564b6f
Vlad <vsakayafignavlad@gmail.com>
2021-01-11 23:19:37 +0300

    Article model

--- myblog/myblog/apps/articles/models.py
+++ myblog/myblog/apps/articles/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 
 class Article(models.Model):
-    pass
+    article_title = models.CharField('Title name', max_length=200)
+    article_text = models.TextField('Title text')
+    article_author = models.CharField('Author Name')
+    pub_date = models.DateTimeField('publication date')
+
 
 
 class Comment(models.Model):
