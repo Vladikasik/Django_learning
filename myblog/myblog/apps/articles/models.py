4d4a46c668b69bbdaf0a3aa0ec4d696d15d80334
Vlad <vsakayafignavlad@gmail.com>
2021-01-12 14:09:35 +0300

    __str__ defs

--- myblog/myblog/apps/articles/models.py
+++ myblog/myblog/apps/articles/models.py
@@ -6,9 +6,14 @@ class Article(models.Model):
     article_text = models.TextField('Title text')
     pub_date = models.DateTimeField('publication date')
 
+    def __str__(self):
+    	return self.article_title
 
 
 class Comment(models.Model):
     parent_article = models.ForeignKey(Article, on_delete=models.CASCADE)
     author_name = models.CharField("Author's name", max_length=50)
     comment_text = models.CharField('Comment text', max_length=300)
+
+    def __str__(self):
+    	return self.author_name
