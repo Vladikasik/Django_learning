e837f9ba07e116527f090380f53148f23b46d7a5
vladikasik <vsakayafignavlad@gmail.com>
2021-01-16 14:46:59 +0300

    articles links

--- myblog/myblog/apps/articles/urls.py
+++ myblog/myblog/apps/articles/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'articles'
 urlpatterns = [
     path('', views.index, name='home'),
     path('<int:article_id>/', views.detail, name='detail'),
